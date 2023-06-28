a7c07db79e2fe54e01f36457f730f8d13ea67dce
anhduc151 <leanhduc1510@gmail.com>
2023-06-28 22:44:22 +0700

    update search in reactjs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import Home from "../pages/Home";
 import Following from "../pages/Following";
 import Profile from "../pages/Profile";
 import Upload from "../pages/Upload";
+import Search from "../pages/Search";
 import HeaderOnly from "../components/Layout/HeaderOnly";
 
 const publicRoutes = [
@@ -9,6 +10,7 @@ const publicRoutes = [
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
     { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
 ]
 
 const privateRoutes = [
